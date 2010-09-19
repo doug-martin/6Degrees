2b2b2b08d279dcdca0721a19383354f458e43896
jamiltron <justinanthonyhamilton@gmail.com>
2010-09-19 00:49:39 -0500

    On branch jamiltron  Changes to be committed: 	modified:   IGRAPH.h

--- IGRAPH.h
+++ IGRAPH.h
@@ -1,23 +1,25 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 #include <iostream>
-#include <set>
 #include <string>
-#include <map>
 #include <iterator>
+#include <unordered_map>
+
 
 using namespace std;
+typedef unordered_multimap<double, double> mapType;
 
 class GRAPH
 {
     public:
-    map<double, set<double> > adjacencyMap;
+    mapType adjacencyMap;
     int V, E;
 
     GRAPH();
-    bool contains(double);
-    bool insertVertex(double);
-    bool insertEdge(double, double);
+    void insertEdge(double, double);
+    void removeEdge(double, double);
+    void printMap();
+
 };
 
 GRAPH::GRAPH()
@@ -26,59 +28,34 @@ GRAPH::GRAPH()
     E = 0;
 }
 
-bool GRAPH::contains(double vert)
+void GRAPH::insertEdge(double e1, double e2)
 {
-    bool flag = false;
-    if (V)
-    {
-        map<double, set<double> >::iterator iter = adjacencyMap.find(vert);
-        if (iter != adjacencyMap.end())
-            flag = true;
-        else
-            flag = false;
-    }
-    
-    else
-        flag =  false;
-    return flag;
-}
-
-bool GRAPH::insertVertex(double vert)
-{
-    bool flag = false;
-    if (contains(vert))
-        flag = false;
-    else
-    {
-        set<double> newSet;
-        adjacencyMap.insert(make_pair(vert, newSet));
-        V++;
-        flag = true;
-    }
-    return flag;
-}
-
-bool GRAPH::insertEdge(double e1, double e2)
-{
-    bool flag = false;
-    if (!contains(e1))
-        flag = insertVertex(e1);
-    if (!contains(e2))
-        flag = insertVertex(e2);
-
-    map<double, set<double> >::iterator iter = adjacencyMap.find(e1);
-    set<double> setCopy = iter->second;
-    setCopy.insert(e2);
-    set<double>::iterator Siter = setCopy.begin();
-    adjacencyMap.insert(make_pair(e1, setCopy));
-
-    iter = adjacencyMap.find(e2);
-    setCopy = iter->second;
-    setCopy.insert(e1);
-    adjacencyMap.insert(make_pair(e2, setCopy));
-
+    adjacencyMap.insert(mapType::value_type(e1, e2));
     E++;
-    return flag;
 }
 
+void GRAPH::removeEdge(double e1, double e2)
+{
+    double toRemove;
+    do
+    {
+        mapType::Const_Iterator it = adjacencyMap.find(e1);
+        toRemove = it->second;
+        if (e2 != toRemove)
+        {
+            ++it;
+        }
+        else
+            adjacencyMap.erase(it);
+    } while(e2 != toRemove);
+}
+
+void GRAPH::printMap()
+{
+    for(mapType::const_iterator it = adjacencyMap.begin(); it != adjacencyMap.end(); ++it)
+    {
+        cout << "[" << it->first << ", " << it->second << "] ";
+    }
+    cout << endl;
+}
 #endif
